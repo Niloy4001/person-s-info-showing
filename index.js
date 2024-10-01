953bf83f12585ee80c1f1b0965db98d69b9441b2
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-01 11:47:56 +0600

    name show after api call function added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const nameList = document.getElementById('name-list-div');
 // console.log(nameList);
 
 const displayName = (data) => {
-    
+    const nameList = document.getElementById('name-list-div');
+    for(let item of data){
+        nameList.innerHTML += `
+             <li class="bg-white rounded-md text-gray-500 p-2 cursor-pointer font-semibold"><span>${item.name}</span></li>
+        `
+        
+    }
 };
 
 
